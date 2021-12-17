2d47fd7e80a58e5120087ef84fc5ef11bfa6b363
alexander.a.volkov@rambler.ru <alexander.a.volkov@rambler.ru>
2021-12-17 18:21:19 +0300

    законченная версия

--- ConsoleAppLesson14/Program.cs
+++ ConsoleAppLesson14/Program.cs
@@ -13,11 +13,16 @@ namespace ConsoleAppLesson14
             Cat cat = new Cat();
             cat.ShowInfo();
             Console.ReadKey();
+            Console.WriteLine();
+
+            Dog dog = new Dog();
+            dog.ShowInfo();
+            Console.ReadKey();
         }
     }
     abstract class Animal
     {
-        public abstract string NameAnimal { get; }
+        public abstract string NameAnimal { get; set; }
         public abstract void Say();
 
         public void ShowInfo()
@@ -25,11 +30,35 @@ namespace ConsoleAppLesson14
             Console.WriteLine("Животное называется {0}", NameAnimal);
             Say();
         }
+        public Animal(string nameAnimal = "Неведома зверюга")
+        {
+            NameAnimal = nameAnimal;
+        }
     }
     class Cat : Animal
     {
         public static string voice = "МЯУ";
-        public override string NameAnimal { get; } = "Кошка";
+        public override string NameAnimal { get; set; }
+
+        public Cat()
+        {
+            NameAnimal = "Кошка";
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Животное говорит {0}", voice);
+        }
+
+    }
+    class Dog : Animal
+    {
+        public static string voice = "ГАВ";
+        public override string NameAnimal { get; set; }
+
+        public Dog()
+        {
+            NameAnimal = "Собака";
+        }
         public override void Say()
         {
             Console.WriteLine("Животное говорит {0}", voice);
